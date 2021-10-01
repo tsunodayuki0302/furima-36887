c2dd8e7c805d65cf89c901fa23c8233d45dd57a7
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-10-01 21:53:59 +0900

    商品出品機能修正２

--- app/javascript/selling_price.js
+++ app/javascript/selling_price.js
@@ -4,7 +4,6 @@ window.addEventListener('load', () => {
     const addTaxDom = document.getElementById('add-tax-price');
     addTaxDom.innerHTML = Math.round(priceInput.value * 0.1 );
     const addPofitDom = document.getElementById("profit");
-    // console.log(priceInput.innerHTML);
     addPofitDom.innerHTML = Math.round(priceInput.value - Math.round(priceInput.value * 0.1 ))
   })
 })
