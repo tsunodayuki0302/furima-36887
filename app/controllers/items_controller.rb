3fa60e20d7ef498ff90ede50765cd73aec4d6589
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-10-04 16:09:27 +0900

    商品編集修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, only: [:new, :edit]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -24,8 +24,7 @@ class ItemsController < ApplicationController
 
   def edit
     @item = Item.find(params[:id])
-    if user_signed_in? && current_user.id == @item.user_id
-    else
+    if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
