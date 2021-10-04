6754a6613f1d8dac60b5d9b8c14c4c73c44f70ea
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-10-04 18:12:50 +0900

    商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :edit]
-  before_action :set_item, only: [ :edit, :update, :show]
+  before_action :set_item, only: [ :edit, :update, :show, :destroy]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -36,6 +36,11 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    @item.destroy
+    redirect_to root_path
+  end
+
   private
   def item_params
     params.require(:item).permit(:product_name, :product_description, :category_id, :product_condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :selling_price, :image).merge(user_id: current_user.id)
@@ -44,7 +49,4 @@ class ItemsController < ApplicationController
   def set_item
     @item = Item.find(params[:id])
   end
-
-
-
 end
