fa26af5c6cbb3a84b34d6f3e977eff5b1d5b0047
tsunodayuki0302 <tsunoda.yuki0302@gmail.com>
2021-10-12 13:00:01 +0900

    日本語表示

--- app/models/item.rb
+++ app/models/item.rb
@@ -11,11 +11,11 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
 
   validates :image, :product_name, :product_description, :category_id, :product_condition_id, :shipping_charge_id, :prefecture_id, :days_to_ship_id, :selling_price, presence: true
-  validates :category_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :product_condition_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :shipping_charge_id,  numericality: { other_than: 1, message: "can't be blank"}
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :days_to_ship_id, numericality: { other_than: 1, message: "can't be blank"}
-  validates :selling_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is invalid"}
+  validates :category_id, numericality: { other_than: 1, message: "を入力してください"}
+  validates :product_condition_id, numericality: { other_than: 1, message: "を入力してください"}
+  validates :shipping_charge_id,  numericality: { other_than: 1, message: "を入力してください"}
+  validates :prefecture_id, numericality: { other_than: 1, message: "を入力してください"}
+  validates :days_to_ship_id, numericality: { other_than: 1, message: "を入力してください"}
+  validates :selling_price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "を¥300〜9,999,999に訂正してください"}
 end
 
